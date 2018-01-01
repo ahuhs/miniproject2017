af3f56c37af92b01d97b76e569434d9c27585f48
aravindmj97 <aravindmj97@gmail.com>
2018-01-01 23:21:03 +0530

    Google Maps and Firebase with Geofire Added

--- app/src/main/java/com/a4dotsinc/profilo/MainActivity.java
+++ app/src/main/java/com/a4dotsinc/profilo/MainActivity.java
@@ -1,20 +1,25 @@
 package com.a4dotsinc.profilo;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         select();
         bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_tabs);
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.floatingActionButton);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -31,15 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
                 if (id == R.id.item_one) {
                     fragment = new MapFrag();
+                    floatingActionButton.setVisibility(View.VISIBLE);
+                    floatingActionButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_place_add));
+                    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent tomap = new Intent(MainActivity.this, MapsActivity.class);
+                            startActivity(tomap);
+                        }
+                    });
                 } else if (id == R.id.item_two) {
 
                     fragment = new TimerFrag();
+                    floatingActionButton.setVisibility(View.VISIBLE);
+                    floatingActionButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_add));
                 } else if (id == R.id.item_three) {
 
                     fragment = new WishlistFrag();
+                    floatingActionButton.setVisibility(View.VISIBLE);
+                    floatingActionButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_add));
                 } else if (id == R.id.item_four) {
 
                     fragment = new SettingsFrag();
+                    floatingActionButton.setVisibility(View.GONE);
                 }
 
                 if (fragment != null){
